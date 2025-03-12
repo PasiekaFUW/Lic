982ed967e7ccee91d55f4a71399c95c94eacd7a4
“Grzegorz <g.jedrzejows@student.uw.edu.pl>
2025-03-12 12:44:52 +0100

    CurrentlyNotWorkinglic.py

--- lic.py
+++ lic.py
@@ -3,10 +3,13 @@ import os
 import sys
 import subprocess
 
+#GJ
 #PPG
 from Configuration.Eras.Era_Phase2C17I13M9_cff import Phase2C17I13M9
 
-#process = cms.Process('OmtfTree')
+#process = cms.Process('OmtfTree') #GJ Comment
+
+#GJ
 #PPG
 process = cms.Process('Analysis',Phase2C17I13M9)
 
@@ -17,15 +20,10 @@ process = cms.Process('Analysis',Phase2C17I13M9)
 # (there is 255 file limit though). Can be empty for crab.
 #
 
-#Sample used up to zad 7
+#GJ
 #dataDir='/scratch_cmsse/akalinow/CMS/Data/SingleMu/13_1_0_04_01_2024/SingleMu_ch0_OneOverPt_Run2029_13_1_0_04_01_2024/13_1_0_04_01_2024/240104_104036/0000/'
-#Sample used from zad 7 including
 dataDir='/scratch_cmsse/akalinow/CMS/Data/SingleMu/13_1_0_11_03_2024/SingleMu_ch0_OneOverPt_Run2023_13_1_0_11_03_2024/13_1_0_11_03_2024/240311_101428/0000/'
-#Sample used for propagation not working due to lacking objects of PSimHit
-#dataDir='/scratch_cmsse/akalinow/CMS/Data/SingleMu/13_1_0_13_02_2024/SingleMu_ch0_OneOverPt_Run2029_13_1_0_13_02_2024/13_1_0_13_02_2024/240213_094022/0000/'
-#PPG
-#dataDir='/eos/cms/store/group/dpg_trigger/comm_trigger/L1Trigger/OMTF/13_1_0_11_03_2024/SingleMu_ch0_OneOverPt_Run2023_13_1_0_11_03_2024/13_1_0_11_03_2024/240311_101428/0000/'
-lsCommand='ls -1 '+dataDir+'|grep root | grep m_99' 
+lsCommand='ls -1 '+dataDir+'|grep root | grep m_99'
 
 print ('command: ',lsCommand)
 lsOutput= subprocess.Popen(lsCommand, stdout=subprocess.PIPE, shell=True, text=True).communicate()[0]
@@ -34,39 +32,30 @@ for f in lsOutput.split():
   files.append('file:'+dataDir+f)
 print ("Number of files in direcotry:",dataDir," ---> ", len(files))
 
+
 process.source = cms.Source("PoolSource", 
-#fileNames = cms.untracked.vstring(
-# 'root://xrootd-cms.infn.it//store/data/Run2023B/Muon0/RAW-RECO/ZMu-PromptReco-v1/000/367/079/00000/b01b794e-9075-4d42-b273-85b2bc66f13a.root',
-#  ),
-fileNames = cms.untracked.vstring(files),
+fileNames = cms.untracked.vstring(files), #GJ
+#fileNames = cms.untracked.vstring( 'file:data.root',),
+#fileNames = cms.untracked.vstring('file:/eos/cms/store/group/dpg_trigger/comm_trigger/L1Trigger/OMTF/PrivateProductionForOMTFStudy/13_1_0_03_04_2024/SingleMu_ch0_OneOverPt_Run2029_13_1_0_03_04_2024/13_1_0_03_04_2024/240403_080928/0000/SingleMu_OneOverPt_1_100_m_1.root'),      
+#fileNames = cms.untracked.vstring(files),
 #skipEvents =  cms.untracked.uint32(220)
 )
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(44000) ) #Number of Events, Max == 44000
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(44000) ) #GJ
 
 #
-# import of standard configurations
-''' Before #PPG
+# import of standard configurationsI
+#
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-#process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.EventContent.EventContent_cff') #GJ uncomment (UC)
 #process.load('Configuration.Geometry.GeometryDB_cff')
-process.load('Configuration.Geometry.GeometryExtended2023Reco_cff')
-process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
-process.load('Configuration.StandardSequences.EndOfProcess_cff')
-'''
-#PPG
-process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.load('Configuration.EventContent.EventContent_cff')
 #process.load('Configuration.Geometry.GeometryExtended2023Reco_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D99Reco_cff')
+#process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+#process.load('Configuration.Geometry.GeometryExtended2026D95Reco_cff')
+process.load('Configuration.Geometry.GeometryExtendedRun4D95Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff") #GJ added
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
-
-
-
 
 
 #
@@ -74,17 +63,31 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 #
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #process.GlobalTag.globaltag = '130X_dataRun3_Prompt_v3'
-#PPG
-process.GlobalTag.globaltag = '131X_mcRun4_realistic_v7'
+#from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+process.GlobalTag.globaltag = '141X_mcRun4_realistic_v3'
 
 #
 # message logger
 #
 process.load('FWCore.MessageService.MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 4000 #mozna modyfikowac 
+process.MessageLogger.cerr.FwkReport.reportEvery = 4000 #GJ
 process.MessageLogger.cerr.enableStatistics = False
 process.MessageLogger.cout.enable =False 
 process.MessageLogger.suppressWarning  = cms.untracked.vstring('*')
+
+# Calibrate Digis
+process.load("L1Trigger.DTTriggerPhase2.CalibratedDigis_cfi")
+process.CalibratedDigis.dtDigiTag = "simMuonDTDigis" 
+process.CalibratedDigis.scenario = 0
+
+# DTTriggerPhase2
+process.load("L1Trigger.DTTriggerPhase2.dtTriggerPhase2PrimitiveDigis_cfi")
+process.dtTriggerPhase2PrimitiveDigis.debug = False
+process.dtTriggerPhase2PrimitiveDigis.dump = False
+process.dtTriggerPhase2PrimitiveDigis.scenario = 0
+#Warto dodac ponizsze co wylacza "coincidence filter". Jak to dizala - MK?
+#process.dtTriggerPhase2PrimitiveDigis.co_option = -1
 
 process.licDigiAnalysis = cms.EDAnalyzer("LicDigiAnalysis",
 
@@ -92,11 +95,22 @@ process.licDigiAnalysis = cms.EDAnalyzer("LicDigiAnalysis",
   srcDTTh_leg = cms.InputTag('simDtTriggerPrimitiveDigis'),
 )
 
+process.out = cms.OutputModule("PoolOutputModule", 
+   fileName = cms.untracked.string("superprimitives2.root"),
+   outputCommands = cms.untracked.vstring(
+                        "drop *",
+                        "keep *_*_*_OmtfTree",
 
-process.digi_step =  cms.Path(process.licDigiAnalysis)
+     )  
+)
+
+process.digi_step =  cms.Path(process.CalibratedDigis * process.dtTriggerPhase2PrimitiveDigis * process.licDigiAnalysis)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.schedule = cms.Schedule(process.digi_step, process.endjob_step)
 
+
+#process.output_step=cms.EndPath(process.out)
+#process.schedule.extend([process.output_step])
 
 #print process.dumpPython();
 #print process.schedule
